8bc90041082341484c97040a9f9a9bb1f90f93f0
John <johnwilles@gmail.com>
2018-03-24 16:02:41 -0400

    Add meta command handler

--- db.c
+++ db.c
@@ -11,6 +11,16 @@ struct InputBuffer_t {
 
 typedef struct InputBuffer_t InputBuffer;
 
+enum Meta_Command_Result_t {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+};
+
+typedef enum Meta_Command_Result_t MetaCommandResult;
+
+enum Prepare_Result_t { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT };
+typedef enum Prepare_Result_t PrepareResult;
+
 InputBuffer* new_input_buffer() {
   InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
   input_buffer->buffer = NULL;
@@ -18,6 +28,14 @@ InputBuffer* new_input_buffer() {
   input_buffer->input_length = 0;
 
   return input_buffer;
+}
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
 }
 
 void print_prompt() { printf("db > "); }
@@ -35,6 +53,10 @@ void read_input(InputBuffer* input_buffer) {
   input_buffer->buffer[bytes_read - 1] = 0;
 }
 
+
+
+
+
 int main(int argc, char* argv[]) {
   InputBuffer* input_buffer = new_input_buffer();
   while (true) {
@@ -42,11 +64,24 @@ int main(int argc, char* argv[]) {
     read_input(input_buffer);
 
     if (strcmp(input_buffer->buffer[0], ".") == 0){
-      switch (do_meta_command(input_buffer))
+      switch (do_meta_command(input_buffer)) {
         case (META_COMMAND_SUCCESS):
           continue;
         case (META_COMMAND_UNRECOGNIZED_COMMAND):
           printf("Unrecognized command '%s'\n", input_buffer->buffer);
+      }
     }
+
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Unrecognized keyword at the start of statement '%s'\n", input_buffer->buffer);
+        continue; 
+    }
+
+    execute_statement(&statement);
+    printf("Executed");
   }
 }
