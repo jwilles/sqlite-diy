704c1162dd9bb017003285d1d5960b760d534fa7
John <johnwilles@gmail.com>
2018-04-08 14:51:15 -0400

    Add working in memory insert and select

--- db.c
+++ db.c
@@ -3,27 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
-
-const uint32_t ID_SIZE = size_of_attribute(Row, id);
-const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
-const uint32_t EMAIL_SIZE = size_of_attribure(Row, email);
-const uint32_t ID_OFFSET = 0;
-const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
-const uint32_t PAGE_SIZE = 4096;
-const uint32_t TABLE_MAX_PAGES = 100;
-const uint32_t ROWS_PER_PAGE = PAGE_SIZE/ROW_SIZE;
-const uint32_t TABLE_MAX_ROWS = TABLE_MAX_PAGES * ROWS_PER_PAGE;
 const uint32_t COLUMN_USERNAME_SIZE = 32;
 const uint32_t COLUMN_EMAIL_SIZE = 255;
-
-struct Table_t {
-  void* pages[TABLE_MAX_PAGES]
-  uint32_t num_rows;
-};
-typedef struct Table_t Table;
 
 struct Row_t {
   uint32_t id;
@@ -65,7 +46,27 @@ struct Statement_t {
 };
 typedef struct Statement_t Statement; 
 
-enum ExecuteResult_t { Execute_Success, Execute_Table_Full};
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+const uint32_t PAGE_SIZE = 4096;
+const uint32_t TABLE_MAX_PAGES = 100;
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE/ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = TABLE_MAX_PAGES * ROWS_PER_PAGE;
+
+struct Table_t {
+  void* pages[TABLE_MAX_PAGES];
+  uint32_t num_rows;
+};
+typedef struct Table_t Table;
+
+enum ExecuteResult_t { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL};
 typedef enum ExecuteResult_t ExecuteResult;
 
 void serialize_row(Row* source, void* destination) {
@@ -129,7 +130,11 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
   return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
-ExceuteResult execute_insert(Statement* statement, Table* table) {
+void print_row(Row* row) {
+  printf("%d, %s, %s \n", row->id, row->username, row->email);
+}
+
+ExecuteResult execute_insert(Statement* statement, Table* table) {
   if (table->num_rows >= TABLE_MAX_ROWS) {
     return EXECUTE_TABLE_FULL;
   }
@@ -144,7 +149,7 @@ ExceuteResult execute_insert(Statement* statement, Table* table) {
 ExecuteResult execute_select(Statement* statement, Table* table) {
   Row row;
   for (uint32_t i = 0; i < table->num_rows; i++) {
-    deserialize_row(row_slot(table, i), Row);
+    deserialize_row(row_slot(table, i), &row);
     print_row(&row); 
   }
   return EXECUTE_SUCCESS;
@@ -154,7 +159,7 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
   switch (statement->type) {
     case (STATEMENT_INSERT):
       return execute_insert(statement, table);
-    case (STAEMENT_SELECT):
+    case (STATEMENT_SELECT):
       return execute_select(statement, table);
   }
 }
@@ -164,10 +169,6 @@ Table* new_table() {
   table->num_rows = 0;
 
   return table;
-}
-
-void print_row(Row* row) {
-  printf("%d, %s, %s \n", row->id, row->username, row->email);
 }
 
 void print_prompt() { printf("db > "); }
